8b8882adfbb208089bfc91fa273aa52bf1f639c4
Nikos Vasilakis <nikos@vasilak.is>
2016-10-11 22:26:52 -0400

    Update scripts

--- scripts/release.sh
+++ scripts/release.sh
@@ -1,14 +1,15 @@
 #!/bin/bash
 
 echo $(pwd)
-cp -r andromeda/ releases/
-cd releases/andromeda
-git pull
+cd releases
+git clone git@github.com:andromeda/andromeda.git
+cd andromeda
 rev=$(git rev-parse --short HEAD)
+echo "rev: $rev"
 sed "s/.*srev.*/    \"srev\": \"$rev\",/" ./package.json > tmp && mv tmp package.json
 # should be almost the same as v=$(git describe --abbrev=0)
 v=$(grep version ./package.json | sed "s/^.*\"version\":[ ]*\"\(.*\)\".*$/\1/")
-rm -rf .git .gitignore circle.yml 
+rm -rf .git .gitignore circle.yml .jshintrc
 cd ..
 tar -cvzf andromeda-$v.tar.gz andromeda/
 rm -rf andromeda/
